0a3889fd764dd878a1a84e9d9f4d2a9613b6503f
Romanova <romanove.e.s@effective-group.ru>
2019-10-07 20:04:04 +0300

    добавление продолжение

--- fixture/project.py
+++ fixture/project.py
@@ -4,9 +4,25 @@ class ProjectHelper:
     def __init__(self, app):
         self.app=app
 
-    def add_progect(self, project):
+    def add_project(self, project):
         wd = self.app.wd
+        self.open_project_page()
+        wd.find_element_by_xpath("//button[contains(text(),'Создать новый проект')]")
+        wd.find_element_by_id('project-name').click()
+        wd.find_element_by_id('project-name').send_keys(project.name)
+        wd.find_element_by_id('project-description').click()
+        wd.find_element_by_id('project-description').send_keys(project.description)
+        wd.find_element_by_xpath("//input[@value='Добавить проект']").click()
 
-    def get_progect_list(self):
-        pass
-    
+    project_cache = None
+
+    def get_project_list(self):
+        if self.project_cache is None:
+            wd = self.app.wd
+            self.open_project_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector('span.group'):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute('value')
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
--- test1/test_add_project.py
+++ test1/test_add_project.py
@@ -3,8 +3,8 @@ from model.project import Project
 
 def test_add_project(app, json_project):
     project = json_project
-    old_project = app.project.get_progect_list()
-    app.project.add_progect(project)
-    new_project = app.project.get_progect_list()
+    old_project = app.project.get_project_list()
+    app.project.add_project(project)
+    new_project = app.project.get_project_list()
     old_project.append(project)
     assert sorted(old_project, key=Project.id_or_max) == sorted(new_project, key=Project.id_or_max)
