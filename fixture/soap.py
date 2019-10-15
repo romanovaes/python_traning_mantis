209c7c9ff008887f86f0fb4f12e1e1fd6719ea96
Romanova <romanove.e.s@effective-group.ru>
2019-10-15 18:39:58 +0300

    исправление-изменение способа указания адреса  к wsdl

--- fixture/soap.py
+++ fixture/soap.py
@@ -9,7 +9,7 @@ class SoapHelper:
         self.app=app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-2.22.1/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.baseUrl+"/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -17,7 +17,7 @@ class SoapHelper:
             return False
 
     def get_list_project(self, username, password):
-        client = Client("http://localhost/mantisbt-2.22.1/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.baseUrl+"/api/soap/mantisconnect.php?wsdl")
         try:
             all_project = client.service.mc_projects_get_user_accessible(username, password)
             list_project = []
